50983465173711da55a192736e0064e692b12e76
migajewski <gajes08@hotmail.com>
2017-09-23 13:30:54 +0200

    appveyor test

---
+++ Solution1/AppVeyorTests/Class1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppVeyorTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            int a = 1;
+
+            Assert.That(a, Is.EqualTo(1));
+        }
+    }
+}
